bfca54473753988a5b7f501cb68bc0184f039019
evgeny.bykanov <red_9894@mail.ru>
2020-03-26 17:20:51 +0400

    коммит

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,20 +2,20 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(double height) {
-        double rsl =height * 1.15;
+        double rsl = height * 1.15;
         return rsl;
     }
 
 
     public static double womanWeight(double height) {
-        double rs2 = height* 1.15;
+        double rs2 = height * 1.15;
         return rs2;
     }
 
     public static void main(String[] args) {
-        double man = Fit.manWeight(175-100);
+        double man = Fit.manWeight(175 - 100);
         System.out.println("Man is " + man);
-        double woman = Fit.womanWeight(175-110);
+        double woman = Fit.womanWeight(175 - 110);
         System.out.println("Woman is " + woman);
 
     }

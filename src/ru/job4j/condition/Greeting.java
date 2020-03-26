5a11217f70bde50fec167964f774e57c5ccbb79a
evgeny.bykanov <red_9894@mail.ru>
2020-03-26 16:53:07 +0400

    9. Площадь треугольника.[#261111]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+       String idea = "I like Java!" + "But I am a newbie.";
+        String separate = " - ";
+        int year = 2020;
+        String id = idea + separate + year;
+        System.out.println(id);
+
+
+    }
+}
+

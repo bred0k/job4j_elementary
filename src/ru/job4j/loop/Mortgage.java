31bb6c07c83a443de7c660903720c3105d192c82
evgeny.bykanov <red_9894@mail.ru>
2020-04-10 17:12:48 +0400

    Ипотека.[#261148]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 1;
+        double debt = amount + amount * percent / 100;
+        while (salary < debt) {
+            year++;
+            debt = (debt - salary) + (debt - salary) * percent / 100;
+        }
+        return year;
+    }
+
+}
+

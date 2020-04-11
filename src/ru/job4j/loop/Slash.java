778a9e864c2ac11859afc8966d2569ca0193785d
evgeny.bykanov <red_9894@mail.ru>
2020-04-11 11:58:13 +0400

    Крест в псевдографике. [#261150]

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -20,7 +20,7 @@ public class Slash {
         draw(3);
         System.out.println("Draw by 5");
         draw(5);
-        System.out.println("Draw by ");
+        System.out.println("Draw by 10");
         draw(10);
     }
 }

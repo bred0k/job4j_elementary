e28c30d9ef46d92772848cbc71cb7d6904131dba
evgeny.bykanov <red_9894@mail.ru>
2020-04-11 15:11:45 +0400

    Классический поиск перебором.[#261079]

---
+++ src/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {7, 5, 10, 3};
+        int value = 10;
+        int result = find.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas5Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {7, 5, 10, 3};
+        int value = 7;
+        int result = find.indexOf(input, value);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+}

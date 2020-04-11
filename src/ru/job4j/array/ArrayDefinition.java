b06fe701585cb417bf335b6f5d28a934d625ce83
evgeny.bykanov <red_9894@mail.ru>
2020-04-11 13:11:39 +0400

    6.0.2. Заполнение массива.[#261093]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,8 +1,23 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    public static void main2(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
 
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Evgenyi Bykanov";
+        names[1] = "Svetlana";
+        names[2] = "Irina";
+        names[3] = "Ekaterina";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
 }
+

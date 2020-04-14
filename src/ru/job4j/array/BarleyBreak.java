12159498cb7c5180fdc7c1ed890506ba60ba79c1
evgeny.bykanov <red_9894@mail.ru>
2020-04-14 15:42:13 +0400

    Двухмерный массив. Таблица умножения.[#261080]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] table = new int[3][3];
+        int temp = 1;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                table[i][j] = temp++;
+            }
+        }
+    }
+}

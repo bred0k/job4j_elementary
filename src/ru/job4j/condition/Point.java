6e511e90064f7f47de547eef9e30abd4d6d46cfa
evgeny.bykanov <red_9894@mail.ru>
2020-05-04 17:05:28 +0400

    Расстояние между точками в трехмерном пространстве.[#261233]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -6,12 +6,17 @@ import static java.lang.Math.pow;
 public class Point {
 
     private int x;
-
     private int y;
+    private int z;
 
-    public Point(int first, int second) {
+    public Point(int first,int second, int third) {
         this.x = first;
         this.y = second;
+        this.z = third;
+    }
+
+    public static double distance3d(Point first, Point second) {
+        return sqrt(pow(second.x - first.x, 2) + pow(second.y - first.y, 2) + pow(second.z - first.z,2));
     }
 
     public double distance(Point that) {
@@ -19,10 +24,12 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        Point a = new Point(5, 4);
-        Point b = new Point(2, 8);
+        Point a = new Point(5, 4,9 );
+        Point b = new Point(2, 8,5);
         double dist = a.distance(b);
         System.out.println(dist);
+        double dist3d = a.distance3d(a,b);
+        System.out.println(dist3d);
     }
 }
 
--- test/ru/job4j/PointTest.java
+++ test/ru/job4j/PointTest.java
@@ -9,11 +9,19 @@ public class PointTest {
 
     @Test
     public void distance() {
-        Point a = new Point (5,4);
-        Point b = new Point (2,8);
+        Point a = new Point (5,4,9);
+        Point b = new Point (2,8,5);
         double result = a.distance(b);
         assertThat(result, is(5.0));
 
+
+    }
+    @Test
+    public void distance3d() {
+        Point a = new Point (5,4,9);
+        Point b = new Point (2,8,5);
+        double result = a.distance3d(a,b);
+        assertThat(result, is(6.4031242374328485));
 
     }
 }

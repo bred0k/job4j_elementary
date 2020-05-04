ba5b0b6640361400055a74afa0023c8c6f0ac3ae
evgeny.bykanov <red_9894@mail.ru>
2020-05-04 15:57:01 +0400

    Рефакторинг - Расстояние между точками.[#261231]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,15 +1,28 @@
 package ru.job4j.condition;
 
-public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(5, 4, 2, 8);
-        System.out.println(" Point.distance " + result);
+        Point a = new Point(5, 4);
+        Point b = new Point(2, 8);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
 
--- test/ru/job4j/PointTest.java
+++ test/ru/job4j/PointTest.java
@@ -1,17 +1,19 @@
 package ru.job4j;
 
-import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.condition.Point;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class PointTest {
 
     @Test
     public void distance() {
-        double in = Point.distance(5, 4, 2, 8);
-        double expected = 5;
-        double out = Point.distance(5, 4, 2, 8);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point (5,4);
+        Point b = new Point (2,8);
+        double result = a.distance(b);
+        assertThat(result, is(5.0));
+
 
     }
 }
